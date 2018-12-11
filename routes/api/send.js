056b9473dd859f99ae5c6dbf145880f6bd2bf84f
bmoishe <moishebarnes@gmail.com>
2018-12-11 17:39:18 +0000

    Personalised message

--- routes/api/send.js
+++ routes/api/send.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const accountSid = require('../../config/keys').accountSid;
+const mobileNumber = require('../../config/keys').mobileNumber;
 const authToken = require('../../config/keys').authToken;
 var twilio = require('twilio');
 
@@ -8,12 +9,15 @@ var client = new twilio(accountSid, authToken);
 
 
 router.post('/', (req, res) => {
-  client.messages.create({
-     to: "+447810841484",
-     from: "+441604422099",
-     body: "hello",}, function(err,message) {
-     console.log(message.sid);
-  });
-});
+  var messageBody = " you have been invited from "+req.body.teamName+" event at "+req.body.eventPlace+" on "+req.body.eventDate+" at "+req.body.eventTime+"."
 
+  for (var i = 0; i<2; i++){
+    client.messages.create({
+      to: mobileNumber,
+      from: "+441604422099",
+      body: "Hi "+ req.body.invitees[i].part_name+messageBody,}, function(err,message) {
+        console.log(message.sid);
+      });
+    }
+})
 module.exports = router;
