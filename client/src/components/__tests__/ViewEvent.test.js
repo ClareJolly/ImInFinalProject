570c187e35371652361e0fea8213e5ed02eff43d
Clare Jolly <clarejollyuk@gmail.com>
2018-12-12 17:13:39 +0000

    US6 - view single event and events page links up to this component

---
+++ client/src/components/__tests__/ViewEvent.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme'
+import * as enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
+import ViewEvent from '../ViewEvent';
+
+let mountedViewEvent
+let mocked_props
+beforeEach(()=>{
+mocked_props = {
+  eventPlace : 'London',
+  eventDate : '01/01/2019',
+  eventTime : '00:00',
+  message : 'message',
+  invitees : [{part_name:"abc", part_number:"123"}]
+}
+mountedViewEvent = shallow(<ViewEvent event={mocked_props} response={'this is a test message'}/>)
+// const component = wrapper.Header();
+})
+
+  it('renders without crashing', () => {
+
+    let mountedViewEvent = shallow(<ViewEvent event={mocked_props} response={'this is a test message'}/>)
+
+  })
+
+  it('contains div', () => {
+    const event_text = mountedViewEvent.find('h3');
+    expect(event_text.text()).toEqual('this is a test message')
+
+  })
--- client/src/containers/Events.js
+++ client/src/containers/Events.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import './Events.css';
+import Modal from 'react-modal';
 import EventModal from './EventModal'
+import ViewEvent from '../components/ViewEvent'
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+
 class Events extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state = {modalIsOpen: false,
       events: [],
+      event_list:[]
     }
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+
+
   }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
 
   componentDidMount() {
     fetch('http://localhost:5000/api/db')
@@ -17,14 +53,15 @@ class Events extends Component {
 
     }).then(data => {
       // console.log(data)
+      this.setState({event_list: data})
     let events = data.map((event, index) => {
         return (
 
-          <div key={index} className="event-buttons"  >
+          <button key={index} className="event-buttons"  >
 
             <EventModal index={index} event={event.teamName} invitees={event.invitees} />
 
-          </div>
+          </button>
 
         )
       })
@@ -35,12 +72,26 @@ class Events extends Component {
   render() {
     return (
       <div className="Events">
-        <h2>Select a team</h2>
-        <div >
+        <h2 onClick={this.openModal}>Select a team </h2>
+        {/*<div >
 
         {this.state.events}
+        // {console.log(this.state.event_list)}
 
-        </div>
+        </div>*/}
+        {this.state.event_list.map ((event, index) => {
+          return <div onClick={() => this.props.setEventID(event)} key={index} className="event-buttons">{event.teamName}
+          {/*<Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          ><ViewEvent event={event} response={'testtesttest'}/></Modal>*/}
+          </div>
+
+        })}
+
       </div>
     );
   }
