243fed9b17861742f460f3e10d4ef79453fbd8fe
Clare Jolly <clarejollyuk@gmail.com>
2018-12-18 20:26:28 +0000

    tests - app.js

--- client/src/containers/__tests__/App.test.js
+++ client/src/containers/__tests__/App.test.js
@@ -22,3 +22,56 @@ it('contains a header', () => {
       // console.log(wrapper.state.teamName)
   expect(header).toHaveLength(1);
 });
+
+// setLogout() {
+//     // console.log("toggle?")
+//     sessionStorage.clear()
+//     this.setState({user: '',userID:'', loggedIn:false})
+//     this.showSection('home')
+//     this.toast_logout()
+//
+//   }
+
+  it('sets logout', () => {
+    // sessionStorage.setItem('username','test')
+    const instance = mountedApp.instance();
+    // instance.setState({ currentView: 'home' })
+    instance.setLogout()
+    expect(mountedApp.state('user')).toBe('');
+    expect(mountedApp.state('userID')).toBe('');
+    expect(mountedApp.state('loggedIn')).toBe(false);
+    expect(mountedApp.state('currentView')).toBe('home');
+  })
+it('sets user', () => {
+  // sessionStorage.setItem('username','test')
+  const instance = mountedApp.instance();
+  // instance.setState({ currentView: 'home' })
+  instance.setUser('user','userID')
+  expect(mountedApp.state('user')).toBe('user');
+  expect(mountedApp.state('userID')).toBe('userID');
+  expect(mountedApp.state('loggedIn')).toBe(true);
+})
+
+ it('sets section', () => {
+   // sessionStorage.setItem('username','test')
+   const instance = mountedApp.instance();
+   instance.setState({ currentView: 'home' })
+   instance.showSection('events')
+   expect(mountedApp.state('currentView')).toBe('events');
+ })
+
+    it('checks cookie setting', () => {
+      // sessionStorage.setItem('username','test')
+      const instance = mountedApp.instance();
+      instance.setState({ cookieaccept: false })
+      instance.cookieSet()
+      expect(mountedApp.state('cookieaccept')).toBe(true);
+    })
+
+    it('set eventID', () => {
+      // sessionStorage.setItem('username','test')
+      const instance = mountedApp.instance();
+      instance.setState({ currentView: 'home' })
+      instance.setEventID({event:'test'})
+      expect(mountedApp.state('currentView')).toBe('viewEvent');
+    })
